64949912a512fc0be3ba8fbb44447d5afac901ff
Manraaj Singh <manraaj05@gmail.com>
2023-07-17 19:32:14 -0700

    Update Trig Function Grapher.py

--- Trig Function Grapher.py
+++ Trig Function Grapher.py
@@ -20,14 +20,17 @@ def graph_trig_function():
         y = np.cos(x_rad)
     elif function.lower() == "tan":
         y = np.tan(x_rad)
+    elif not function:
+        print("No function chosen!")
     else:
         print("Invalid trigonometric function.")
         return
 
     # Create the plot
-    plt.plot(x, y)
+    plt.plot(x_rad, y)
     plt.title(f"Graph of {function}({angle_type} angle)")
-    plt.xlabel("Angle")
+    plt.xticks(np.linspace(0, 2 * np.pi, 5), labels=[r'$0$', r'$\frac{\pi}{2}$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'])  # Custom x-axis labels in radians
+    plt.xlabel("Radians")
     plt.ylabel("Value")
     plt.grid(True)
     plt.show()
